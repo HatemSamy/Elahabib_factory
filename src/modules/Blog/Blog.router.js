3f38accff03282b046b509202bcda1bdec136657
hatem <hatim2012003@gmail.com>
2025-05-10 04:20:39 +0300

    Update something

--- src/modules/Blog/Blog.router.js
+++ src/modules/Blog/Blog.router.js
@@ -3,9 +3,11 @@ import * as BlogController from "./controller/Blog.js"
 import { fileValidation, myMulter } from "../../services/multer.js";
 const router=Router()
 
+
+
 router.post("/create",myMulter(fileValidation.image).single("image"),BlogController.createBlog)
 router.get("/All-Blogs",BlogController.getAllBlogs)
-router.get("/getBlog/:id",BlogController.getBlogById)
+
 
 
 export default router
